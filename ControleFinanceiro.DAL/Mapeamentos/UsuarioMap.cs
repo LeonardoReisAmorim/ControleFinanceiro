95151fe5db82ea995d5a25ee5fa7380972064347
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2021-12-09 15:04:47 -0300

    controlv3

---
+++ ControleFinanceiro.DAL/Mapeamentos/UsuarioMap.cs
@@ -0,0 +1,20 @@
+﻿using ControleFinanceiro.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro.DAL.Mapeamentos
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.Property(u => u.Id).ValueGeneratedOnAdd();
+
+            builder.Property(u => u.CPF).IsRequired().HasMaxLength(20);
+            builder.HasIndex(u => u.CPF).IsUnique();
+        }
+    }
+}
